def9d0d3658a2f76e33b6ba3a24a5ce235c17ee0
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-07-05 12:00:23 +0200

    fixed it?

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
-"""API queries post for top 10 posts on subreddit"""
+"""
+Module for querying the Reddit API and printing top posts.
+
+This module contains a function that interacts with the Reddit API
+to retrieve and display the titles of the top 10 hot posts for a given subreddit.
+"""
+
 import requests
 
 
-def top_ten(subreddits):
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
 
-    url = f"https://www.reddit.com/r/AskMen/comments/13c6t9s/what_is_a_gym_bro/about.json"
-    headers = {
-        "User-Agent": "linux:alu-scripting:v1.0.0 (SIPHO WAS HERE MAN)"
-    }
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None. This function prints the results directly.
+
+    If not a valid subreddit, it prints None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'linux:alu-scripting:v1.0.0 (by /u/Mpho_19)'}
 
     try:
-        response = requests.get(url, headers=headers, redirects=False)
+        response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            posts = data["data"]["children"]
+            posts = data['data']['children']
             for post in posts:
-                print(post["data"]["title"])
-            else:
-                print(None)
-
-    # Error exception handling if nothing found
+                print(post['data']['title'])
+        else:
+            print(None)
     except Exception:
         print(None)

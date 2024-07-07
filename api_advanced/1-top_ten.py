1c94333d4f28ea1f85fb1afbfcec715a5df45717
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-07-07 12:57:37 +0200

    help

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+""" top_ten.py """
 import requests
+import sys
 
 
 def top_ten(subreddit):
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    try:
-        response = requests.get(
-            subreddit_url,
-            headers=headers,
-            allow_redirects=False)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{AskMen}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'MyBot/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
         return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
 
-    if response.status_code == 200:
-        try:
-            json_data = response.json()
-            posts = json_data.get('data').get('children', [])
-            if not posts:
-                print(None)
-                return
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title'))
-        except (ValueError, KeyError):
-            print(None)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        print("{:d}".format(top_ten(sys.argv[1])))

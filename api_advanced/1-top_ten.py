e81b3cfb07eff2817b6069b4e79dbb1ea0d8f403
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-07-07 12:59:07 +0200

    hellppp

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,8 @@ import sys
 
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{AskMen}/hot.json?limit=10'.format(subreddit)
+    url = 'https://www.reddit.com/r/{AskMen}/hot.json?limit=10'.format(
+        subreddit)
     headers = {'User-Agent': 'MyBot/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
